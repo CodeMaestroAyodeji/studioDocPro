1f9702bf8a0f3b200eec5c3f8a1fd94d26ea7d80
Ayodeji Folarin Ajuwon <ajuwon2013@gmail.com>
2025-09-30 21:08:03 +0000

    FirebaseError: Failed to get document because the client is offline.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableMultiTabIndexedDbPersistence } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +17,19 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// Enable multi-tab persistence
+if (typeof window !== 'undefined') {
+    enableMultiTabIndexedDbPersistence(db).catch((err) => {
+        if (err.code == 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled in one tab at a time.
+            console.warn('Firestore persistence failed: multiple tabs open.');
+        } else if (err.code == 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable this persistence mode.
+            console.warn('Firestore persistence is not available in this browser.');
+        }
+    });
+}
 
 export { app, auth, db };
