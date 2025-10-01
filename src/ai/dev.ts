23931d5dd14359e150370ccbd893ac372f80effb
Ayodeji Folarin Ajuwon <ajuwon2013@gmail.com>
2025-10-01 05:22:17 +0000

    Error: Failed to parse private key: Error: Too few bytes to read ASN.1 v

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,3 @@
 
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/intelligent-field-completion.ts';
 import '@/ai/flows/list-users';
--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,21 +1,11 @@
 
 import * as admin from 'firebase-admin';
+import {credential as AppHostingCredential} from 'firebase-admin/app';
 
 if (admin.apps.length === 0) {
-    if (process.env.FIREBASE_SERVICE_ACCOUNT_EMAIL && process.env.FIREBASE_PRIVATE_KEY) {
-        admin.initializeApp({
-            credential: admin.credential.cert({
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_SERVICE_ACCOUNT_EMAIL,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-            }),
-        });
-    } else {
-        // Fallback for environments where Application Default Credentials are set, like Google Cloud Run
-        admin.initializeApp({
-            credential: admin.credential.applicationDefault(),
-        });
-    }
+    admin.initializeApp({
+        credential: AppHostingCredential.applicationDefault(),
+    });
 }
 
 export { admin };
