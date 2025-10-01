48230d205068ecb8bb23af636f4424e6b62906ad
Ayodeji Folarin Ajuwon <ajuwon2013@gmail.com>
2025-10-01 05:25:33 +0000

    Try fixing this error: `Console Error: TypeError: Cannot read properties

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,25 @@
 
+'use server';
+
 import * as admin from 'firebase-admin';
-import {credential as AppHostingCredential} from 'firebase-admin/app';
+import { GoogleAuth } from 'google-auth-library';
 
 if (admin.apps.length === 0) {
+    const auth = new GoogleAuth({
+        scopes: [
+            'https://www.googleapis.com/auth/cloud-platform',
+            'https://www.googleapis.com/auth/datastore',
+            'https://www.googleapis.com/auth/devstorage.full_control',
+            'https://www.googleapis.com/auth/firebase',
+            'https://www.googleapis.com/auth/identitytoolkit',
+            'https://www.googleapis.com/auth/userinfo.email',
+        ],
+    });
+
+    const credential = new admin.credential.GoogleAuth(auth);
+
     admin.initializeApp({
-        credential: AppHostingCredential.applicationDefault(),
+        credential,
     });
 }
 
