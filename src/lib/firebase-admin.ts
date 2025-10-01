eb0f8d594c34eef1f1e127d7a76de7c2f7df42be
Ayodeji Folarin Ajuwon <ajuwon2013@gmail.com>
2025-10-01 05:27:10 +0000

    Try fixing this error: `Runtime Error: TypeError: {imported module [exte

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,25 +2,11 @@
 'use server';
 
 import * as admin from 'firebase-admin';
-import { GoogleAuth } from 'google-auth-library';
 
-if (admin.apps.length === 0) {
-    const auth = new GoogleAuth({
-        scopes: [
-            'https://www.googleapis.com/auth/cloud-platform',
-            'https://www.googleapis.com/auth/datastore',
-            'https://www.googleapis.com/auth/devstorage.full_control',
-            'https://www.googleapis.com/auth/firebase',
-            'https://www.googleapis.com/auth/identitytoolkit',
-            'https://www.googleapis.com/auth/userinfo.email',
-        ],
-    });
-
-    const credential = new admin.credential.GoogleAuth(auth);
-
-    admin.initializeApp({
-        credential,
-    });
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
 }
 
 export { admin };
