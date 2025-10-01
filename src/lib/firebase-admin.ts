a2906d540787fbd2a85d75dea7ee921617e81c81
Ayodeji Folarin Ajuwon <ajuwon2013@gmail.com>
2025-10-01 05:20:16 +0000

    TypeError: Cannot read properties of undefined (reading 'INTERNAL')

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,21 @@
+
 import * as admin from 'firebase-admin';
 
 if (admin.apps.length === 0) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-  });
+    if (process.env.FIREBASE_SERVICE_ACCOUNT_EMAIL && process.env.FIREBASE_PRIVATE_KEY) {
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_SERVICE_ACCOUNT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            }),
+        });
+    } else {
+        // Fallback for environments where Application Default Credentials are set, like Google Cloud Run
+        admin.initializeApp({
+            credential: admin.credential.applicationDefault(),
+        });
+    }
 }
 
 export { admin };
